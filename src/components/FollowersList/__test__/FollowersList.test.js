c451566d165554a574e47e56be6156931f9f30ba
Francois Lacoursiere <lacouf@gmail.com>
2024-10-29 20:33:13 -0400

    test asynchronous with backend call

---
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -0,0 +1,29 @@
+import {render, screen} from "@testing-library/react";
+import FollowersList from "../FollowersList";
+import {BrowserRouter} from "react-router-dom";
+
+const MockFollowersList = () => {
+    return (
+        <BrowserRouter>
+            <FollowersList />
+        </BrowserRouter>
+    )
+}
+
+describe('tests de followers', () => {
+
+    it('find 1 follower card', async () => {
+            render(<MockFollowersList />);
+            const followerDivElement = await screen.findByTestId("follower-item-0")
+
+            expect(followerDivElement).toBeInTheDocument();
+        }
+    );
+
+    it('find all follower card', async () => {
+            render(<MockFollowersList />);
+            const followerDivElements = await screen.findAllByTestId(/follower-item-/i)
+            expect(followerDivElements.length).toBe(5);
+        }
+    );
+});
